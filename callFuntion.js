55a3f46b7bad18ff3afe489992a10ce75f304b5b
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-01-16 16:28:57 +0600

    sum of array

--- callFuntion.js
+++ callFuntion.js
@@ -1,7 +1,22 @@
-function sum(num1, num2){
-    const result =  num1 + num2;
-    return result;
+// function sum(num1, num2){
+//     const result =  num1 + num2;
+//     return result;
+// }
+
+// const result = sum(10,20)
+// console.log(result);
+
+function sumOfNumbers(numbers){
+    // console.log(number);
+    let sum = 0;
+    for (const number of numbers){
+        sum = sum + number;
+        // console.log(sum);
+    }
+    return sum;
+
 }
 
-const result = sum(10,20)
-console.log(result);
+const numbs = [4, 1, 4, 1]
+const sum = sumOfNumbers(numbs);
+console.log(sum);
